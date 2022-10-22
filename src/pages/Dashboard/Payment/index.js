fd3c8142f46d2033172e4fefe74a8703e52422e8
Hector-Santos <98065266+Hector-Santos@users.noreply.github.com>
2022-10-22 19:34:20 -0300

    fix(payment/pages): changed location of code for payment page

--- src/pages/Dashboard/Payment/index.js
+++ src/pages/Dashboard/Payment/index.js
@@ -1,3 +1,23 @@
+import Tile from '../../../components/Dashboard/Tile';
+import styled from 'styled-components';
+
 export default function Payment() {
-  return 'Pagamento: Em breve!';
+  return (
+    <>
+      <h1>Ingresso e pagamento</h1>
+      <h2>Primeiro, escolha sua modalidade de ingresso</h2>
+      <Tiles>
+        <Tile price="250,00" title="title"></Tile>
+        <Tile price="250,00" title="title"></Tile>
+      </Tiles>
+    </>
+  );
 }
+
+const Tiles = styled.div`
+  height: 100%;
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  margin-top: 15px;
+  `;
