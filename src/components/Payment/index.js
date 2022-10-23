34c4b7ada3baf53e9d0210abb39718b409cff05b
Victor <vanmatos@poli.ufrj.br>
2022-10-23 11:07:43 -0300

    refactor: isolate ticket selection component

--- src/components/Payment/index.js
+++ src/components/Payment/index.js
@@ -1,113 +1,17 @@
-import axios from 'axios';
-import { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import Typography from '@material-ui/core/Typography';
 
-import Tile from '../Dashboard/Tile';
+import TicketSelect from './TicketSelection';
 
-export default function TicketSelect() {
-  const [categories, setCategories] = useState([]);
-  const [selected, setSelected] = useState();
-  const [category, setCategory] = useState();
-  const [price, setPrice] = useState();
-
-  useEffect(() => {
-    const URL = process.env.REACT_APP_API_BASE_URL;
-    const promise = axios.get(`${URL}/tickets/categories`);
-
-    promise.then((response) => {
-      const { data } = response;
-      setCategories(data);
-    });
-
-    promise.catch((err) => {
-      const message = err.response.statusText;
-      alert(message);
-    });
-  }, []);
-
-  function createTicketCategories() {
-    return categories.map((ticketCategory) => {
-      const { id, category, price } = ticketCategory;
-      return (
-        <Tile
-          key={id}
-          category={category}
-          price={price}
-          active={ticketCategory === selected}
-          onClick={() => selectCategory(ticketCategory)}
-        />
-      );
-    });
-  }
-
-  function selectCategory(ticketCategory) {
-    const { category, price } = ticketCategory;
-
-    setSelected(ticketCategory);
-    setCategory(category);
-    setPrice(price);
-  }
-
-  function createCategorySummary() {
-    if (category === 'Online') {
-      return (
-        <TicketInstruction>
-          <h2>Fechado! O total ficou em R${price}. Agora é só confirmar</h2>
-          <button>RESERVAR INGRESSO</button>
-        </TicketInstruction>
-      );
-    }
-  }
-
-  const ticketCategories = createTicketCategories();
-  const categorySummary = createCategorySummary();
-
+export default function SelectTicketOptions() {
   return (
     <>
       <StyledTypography variant="h4">Ingresso e Pagamento</StyledTypography>
-      <TicketInstruction>
-        <h2>Primeiro, escolha sua modalidade de ingresso</h2>
-      </TicketInstruction>
-      <TicketOptions>{ticketCategories}</TicketOptions>
-      <div>{categorySummary}</div>
+      <TicketSelect />
     </>
   );
 }
 
 const StyledTypography = styled(Typography)`
   margin-bottom: 20px !important;
-`;
-
-const TicketInstruction = styled.div`
-  h2 {
-    font-family: 'Roboto';
-    font-style: normal;
-    font-weight: 400;
-    font-size: 20px;
-    line-height: 23px;
-    color: #8e8e8e;
-    margin: 37px 0 17px 0;
-  }
-
-  button {
-    width: 162px;
-    height: 37px;
-    background: #e0e0e0;
-    box-shadow: 0px 2px 10px rgba(0, 0, 0, 0.25);
-    border-radius: 4px;
-
-    font-family: 'Roboto';
-    font-style: normal;
-    font-weight: 400;
-    font-size: 14px;
-    line-height: 16px;
-    text-align: center;
-
-    color: #000000;
-  }
-`;
-
-const TicketOptions = styled.div`
-  display: flex;
 `;
