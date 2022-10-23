6a5e4e640dcb591b18493b50354558141633f205
Victor <vanmatos@poli.ufrj.br>
2022-10-22 23:07:25 -0300

    feat: render ticket categories from db

--- src/components/Payment/index.js
+++ src/components/Payment/index.js
@@ -1,17 +1,42 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import Typography from '@material-ui/core/Typography';
 
 import Tile from '../Dashboard/Tile';
 
 export default function TicketSelect() {
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    const URL = 'http://localhost:4000/tickets/categories';
+    const promise = axios.get(URL);
+
+    promise.then((response) => {
+      const { data } = response;
+      setCategories(data);
+    });
+
+    promise.catch((err) => {
+      const message = err.response.statusText;
+      alert(message);
+    });
+  }, []);
+
+  function createTicketCategories() {
+    return categories.map((ticketCategory) => {
+      const { id, category, price } = ticketCategory;
+      return <Tile key={id} category={category} price={price} />;
+    });
+  }
+
+  const ticketCategories = createTicketCategories();
+
   return (
     <>
       <StyledTypography variant="h4">Ingresso e Pagamento</StyledTypography>
       <TicketInstruction>Primeiro, escolha sua modalidade de ingresso</TicketInstruction>
-      <TicketOptions>
-        <Tile price="250,00" title="title"></Tile>
-        <Tile price="250,00" title="title"></Tile>
-      </TicketOptions>
+      <TicketOptions>{ticketCategories}</TicketOptions>
     </>
   );
 }
